b65552e2e2056e3d8047db51dcf960d99ee4cd34
John Quinn <jsqpers@gmail.com>
2026-04-24 21:38:22 +0000

    John's test comment

--- programming-project-ex-1/src/Application.cpp
+++ programming-project-ex-1/src/Application.cpp
@@ -3,6 +3,8 @@
 using namespace bobcat;
 
 Application::Application() {
+    // this is a change that I made - John Quinn
+
     window = new Window(25, 75, 400, 400, "Simple Navigation Project");
 
 
