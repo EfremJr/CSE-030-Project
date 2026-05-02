e458ea2a804b3dc8ca8440f35c69c26dbc0839d1
John Quinn <jsqpers@gmail.com>
2026-05-02 22:26:26 +0000

    Added VisualPath struct and visualizePath method to Visualizers.h

--- programming-project-ex-1/inc/Visualizers.h
+++ programming-project-ex-1/inc/Visualizers.h
@@ -36,6 +36,11 @@ public:
     }
 };
 
+struct VisualPath {
+    ArrayList<VertexVisualizer*> vertices;
+    ArrayList<EdgeVisualizer*> edges;
+};
+
 class GraphVisualizer {
 
 public:
@@ -47,17 +52,15 @@ public:
             Vertex* currentVertex = graph->vertices[i];
             //TODO: Calculate x and y for drawing vertex based on amount of vertices
 
-            //VertexVisualizer* visualVertex = new VertexVisualizer(calcX, calcY, currentVertex->data, currentVertex->index)
-            //vertices.append(visualVertex)
-        }
-        for (int i = 0; i < graph->vertices.size(); i++){
-            Vertex* currentVertex = graph->vertices[i];
-            
-            for (int j = 0; j < currentVertex->edgeList.size(); j++){
+            VertexVisualizer* visualVertex = new VertexVisualizer(0, 0, currentVertex->data, currentVertex->index);
+            vertices.append(visualVertex);
+
+            for (int j = 0; j < currentVertex->edgeList.size(); j++) {
                 Edge* currentEdge = currentVertex->edgeList[j];
                 Vertex* destination = currentEdge->to;
 
-                if (currentVertex->index < destination->index){
+                // Condition only works if all edges are two-way
+                if (currentVertex->index > destination->index){
                     VertexVisualizer* v1 = vertices[currentVertex->index];
                     VertexVisualizer* v2 = vertices[destination->index];
                     
@@ -66,7 +69,35 @@ public:
                 }
             }
         }
-    }    
+    }
+
+    VertexVisualizer* visualizedVertex(Vertex* vertex) { return vertices[vertex->index]; }
+    
+    EdgeVisualizer* visualizedEdge(Vertex* vertex1, Vertex* vertex2) {
+        for (int i = 0; i < edges.size(); i++) {
+            if ( (edges[i]->vertex1->cityIndex == vertex1->index && edges[i]->vertex2->cityIndex == vertex2->index)
+                || (edges[i]->vertex1->cityIndex == vertex2->index && edges[i]->vertex2->cityIndex == vertex1->index)) {
+                return edges[i];
+            }
+        }
+        return nullptr;
+    }
+
+    VisualPath visualizePath(Waypoint* endpoint) {
+        VisualPath path;
+
+        Waypoint* temp = endpoint;
+        while (temp->parent != nullptr) {
+            VertexVisualizer* tempVertex = visualizedVertex(temp->vertex);
+            EdgeVisualizer* edge = visualizedEdge(temp->vertex, temp->parent->vertex);
+
+            path.vertices.append(tempVertex);
+            path.edges.append(edge);
+        }
+        path.vertices.append(visualizedVertex(temp->parent->vertex));
+
+        return path;
+    }
 };
 
 #endif
